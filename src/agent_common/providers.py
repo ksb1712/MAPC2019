bc2c08ab3ed8b05633f7d7886e30fef98f7621cb
ksb1712 <you@example.com>
2019-05-26 21:52:29 +0000

    Add persistence to local map
    The local map for each agent is grown w.r.t the agent movement
    and is updated. The previous info from the last update is retained.

--- src/agent_common/providers.py
+++ src/agent_common/providers.py
@@ -3,7 +3,7 @@ import rospy
 import sys
 
 from behaviour_components.sensors import Sensor
-
+from mapc_ros_bridge.msg import Position
 from agent_common.agent_utils import relative_euclidean_distance
 import numpy as np
 
@@ -35,6 +35,9 @@ class PerceptionProvider(object):
         self.number_of_blocks_sensor = Sensor(name="number_of_blocks", initial_value=0)  # TODO this is currently never updated
 
         self.local_map = np.zeros((11,11))
+
+        self.agent = []
+        self.agent_location = Position(5,5) #Agent initial agent position to center of grid (vision range 5)
         #TODO Entity sensor
 
 
@@ -54,7 +57,10 @@ class PerceptionProvider(object):
 
         self._update_obstacles(request_action_msg)  # TODO this could be more sophisticated and potentially extracted like above
 
-        self._update_map()
+        self.agent = request_action_msg.agent
+
+        self.map_status()
+
 
     def _update_dispensers(self, request_action_msg):
         """
@@ -113,13 +119,56 @@ class PerceptionProvider(object):
         self.obstacle_sensor.update(newValue=len(self.obstacles) > 0)
         self.obstacle_sensor.sync()
 
+    def check_vision_range(self,last_direction):
+        
+        x, y = self.local_map.shape
+        if last_direction == "n":
+            if self.agent_location.y < 5:
+                temp = [0 for i in range(y)]
+                self.local_map = np.insert(self.local_map,0,temp,axis=0)
+                self.agent_location.y += 1
+        
+        elif last_direction == "s":
+            if x - self.agent_location.y < 5:
+                temp = [0 for i in range(y)]
+                self.local_map = np.insert(self.local_map,x,temp,axis=0)
+        
+        elif last_direction == "e":
+            if y - self.agent_location.x < 5:
+                temp = [0 for i in range(x)]
+                self.local_map = np.insert(self.local_map,y,temp,axis=1)
+
+        elif last_direction == "w":
+            if self.agent_location.x < 5:
+                temp = [0 for i in range(x)]
+                self.local_map = np.insert(self.local_map,0,temp,axis=1)
+                self.agent_location.x += 1 
+
+
     def _update_map(self):
+        
+        for obstacle in self.obstacles:
+            # print("obstacle  x: {} y: {}".format(obstacle.pos.x,obstacle.pos.y))
+            self.local_map[obstacle.pos.y + self.agent_location.y][obstacle.pos.x + + self.agent_location.x] = 1
+        
+        for dispenser in self.dispensers:
+            self.local_map[dispenser.pos.y + + self.agent_location.y][dispenser.pos.x + + self.agent_location.x] = 2
+            # print("dispenser x: {} y: {}".format(dispenser.pos.x,dispenser.pos.y))
+
+        for block in self.blocks:
+            self.local_map[block.pos.y + + self.agent_location.y][block.pos.x + self.agent_location.x] = 3
+
+        for goal in self.goals:
+            self.local_map[goal.pos.y + self.agent_location.y][goal.pos.x + self.agent_location.x] = 4
+
+        # self.local_map[self.agent_location.y][self.agent_location.x] = 9
+    def map_status(self):
 
         '''
         Update the local map (11 x 11) grid with agent at center. 
         Assumed vision range : 5
 
-        x positive - right
+        x positive - right   (x of agent is y of map)
         y positive - up
 
         0 - empty
@@ -128,21 +177,51 @@ class PerceptionProvider(object):
         3 - block
         4 - goal
         5 - entity
+        9 - agent
+
+        n - go up - agent.y decreases
+        e - go left - agent.x increases
 
         '''
-        self.local_map = np.zeros((11,11))
 
-        for obstacle in self.obstacles:
-            # print("obstacle  x: {} y: {}".format(obstacle.pos.x,obstacle.pos.y))
-            self.local_map[obstacle.pos.y + 5][obstacle.pos.x + 5] = 1
+        """
+        Identify the last successful move direction and change the map accordingly
+        """
+        last_direction = []
+        if self.agent.last_action == "move":
+            if self.agent.last_action_result == "success":
+                last_direction = str(self.agent.last_action_params[0])
+                # print("Move to " + last_direction)
+
+        #Change location
+        #Get percept range and modify map accordingly
+        #Change location if needed
+        #Update map
+
+        if len(last_direction) > 0:
+            if last_direction == 'n':
+                self.agent_location.y -= 1
+            
+            elif last_direction == 's':
+                self.agent_location.y += 1
+
+            elif last_direction == 'e':
+                self.agent_location.x += 1
+
+            elif last_direction == 'w':
+                self.agent_location.x -= 1
+            
+            self.check_vision_range(last_direction)
         
-        for dispenser in self.dispensers:
-            self.local_map[dispenser.pos.y + 5][dispenser.pos.x + 5] = 2
-            # print("dispenser x: {} y: {}".format(dispenser.pos.x,dispenser.pos.y))
-
+        else:
+            last_direction = "failed"
 
         
-        for i in range(11):
-            print(self.local_map[i,:])
+        self._update_map()
+        
+        
+        print("direction: {}, shape: {}".format(last_direction,self.local_map.shape))
+        print("\n")
+        print(self.local_map)
         
         print("\n\n")
